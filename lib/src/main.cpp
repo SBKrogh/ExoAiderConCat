749b0a0fa6c25a670d4391e4dab884d9950a91f5
bomlee <simonkro@gmail.com>
2019-09-25 13:48:31 +0200

    Error handling

---
+++ lib/src/main.cpp
@@ -0,0 +1,42 @@
+#include <Arduino.h>
+#include "BluetoothSerial.h" 
+#include "TaskBT2.h"
+
+BluetoothSerial ESP_BT; //Object for Bluetooth
+TaskBT2 Task(14, 32);   // pins for IMUs
+
+std::vector<float> DataBufferSerial;
+std::vector<uint8_t> DataBufferBT;
+
+void setup() {
+  Task.BeginIMU();    // Initiate IMU 
+  Serial.begin(9600); //Start Serial monitor in 9600
+  
+  ESP_BT.begin("Exo-Aider ESP32"); //Name of Bluetooth Device
+  Serial.println("Bluetooth Device is Ready to Pair");
+
+  pinMode (LED_BUILTIN, OUTPUT);//Specify that LED pin is output
+
+  while(ESP_BT.hasClient() == false){digitalWrite(LED_BUILTIN, LOW);} // Wait until a connection is established
+  Serial.println("Device Connected"); 
+}
+
+
+void loop() {
+  while(ESP_BT.hasClient() == false){digitalWrite(LED_BUILTIN, LOW);} // Check for client, if non, wait
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  if (ESP_BT.available())             // Check for received task over Bluetooth
+  {
+    Task.SetTask(ESP_BT.read());      // Check task to execute
+  }
+
+  if (Task.RunTask()){                // Check if task exsists         
+    Task.ExecuteTask();               // Execute task 
+    DataBufferBT = Task.GetSensorDataBT(); // Get task/sensor data
+    ESP_BT.write(DataBufferBT.data(), DataBufferBT.size()); // Send data over Bluetooth
+    delay(1);
+    
+  }
+}
+
